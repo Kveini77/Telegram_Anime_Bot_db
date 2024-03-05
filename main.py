de57a7f78d9ac5284c2d1a0a96d2c66a3f0c254e
Kveini77 <Kveini77@gmail.com>
2024-03-05 18:53:35 +0600

    FC3

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
 from os import getenv
-
+import logging
 
 load_dotenv()
-bot = Bot(token=getenv('BOT_TOKEN'))
-dp = Dispatcher(bot)
+bot = Bot(token=getenv("BOT_TOKEN"))
+dp = Dispatcher()
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("Привет")
+    await message.answer(message.text)
+
+
 
 
 async def main():
-    await dp.start_polling()
+    # запуск бота
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
